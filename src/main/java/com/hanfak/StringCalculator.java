a644cdf3fce0a7e873c1a19c8efe4c02a360ffae
Hanif Fakira <fakira.work@gmail.com>
2016-12-05 20:13:49 +0000

    Q2 RF extract sum array to private

--- src/main/java/com/hanfak/StringCalculator.java
+++ src/main/java/com/hanfak/StringCalculator.java
@@ -7,14 +7,18 @@ public class StringCalculator {
 		if (input.isEmpty()) {
 			return 0;
 		} 
+		return sumStringArray(numberSet);
+	}
+	
+	private int stringToInt(String input){
+		return Integer.parseInt(input);
+	}
+	
+	private int sumStringArray(String[] numberSet) {
 		int sum = 0;
 		for (String stringNumbers: numberSet) {
 			sum += stringToInt(stringNumbers);
 		}
 		return sum;
-	}
-	
-	private int stringToInt(String input){
-		return Integer.parseInt(input);
 	}
 }
